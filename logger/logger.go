9a509d7887c21cd91375761f0d5c8545b2c3f108
khaki <khaki@khakideMacBook-Pro.local>
2021-03-14 17:59:01 +0800

    add logger

---
+++ logger/logger.go
@@ -0,0 +1,41 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
+)
+
+func InitLogger(logPath string, logLevel string) {
+	hook := lumberjack.Logger{
+		Filename:   logPath, // 日志文件路径
+		MaxSize:    1024,    // megabytes
+		MaxBackups: 3,       // 最多保留3个备份
+		MaxAge:     7,       //days
+		Compress:   true,    // 是否压缩 disabled by default
+	}
+
+	var level zapcore.Level
+	switch logLevel {
+	case "debug":
+		level = zap.DebugLevel
+	case "info":
+		level = zap.InfoLevel
+	case "error":
+		level = zap.ErrorLevel
+	default:
+		level = zap.InfoLevel
+	}
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook)),
+		level,
+	)
+
+	l := zap.New(core)
+	logger = l.Sugar()
+	logger.Info("DefaultLogger init success")
+}
